9f27f6addb4764186c3314eb687fea8f70b9a9f3
cgfever <369414212@qq.com>
2021-05-21 12:37:59 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -4,7 +4,7 @@ const BLOG = {
   email: 'cgfever@gmail.com',
   link: 'https://cc.sres.life',
   description: '知识改变命运',
-  lang: 'en-US',
+  lang: 'zh-cn',
   appearance: 'auto', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
